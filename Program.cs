a5cd48aa8513442777239bc1330a477183b8fa76
admin <admin@CHETUIWK015>
2022-02-23 18:16:21 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 4;
+            int c = a + b;
+            Console.WriteLine(c);
+            
+
+         
+        }
+    }
+}
